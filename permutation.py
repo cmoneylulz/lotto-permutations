78ad5e7c53dd764c5154d1826af373963b646869
cmoneylulz <element4emerica@gmail.com>
2014-08-07 15:04:21 -0400

    cash 4 permutations implemented

--- permutation.py
+++ permutation.py
@@ -17,5 +17,17 @@ def find_cash_3(number_list):
 		find_permutations(list_one, 0)
 		find_permutations(list_two, 0)
 
+def find_cash_4(number_list):
+	if len(number_list) != 4:
+		print "not a cash 4 list"
+	else:
+		list_one = [number_list[0], number_list[1], number_list[2]]
+		list_two = [number_list[1], number_list[2], number_list[3]]
+		find_permutations(list_one, 0)
+		find_permutations(list_two, 0)
+
 test_list = [1,2,3]
 find_cash_3(test_list)
+
+test_list2 = [1,2,3,4]
+find_cash_4(test_list2)
