86053576fc547c5317ed248aceab449d0ca339f4
cmoneylulz <element4emerica@gmail.com>
2014-08-06 22:37:08 -0400

    starting on cash 3 permutations

--- permutation.py
+++ permutation.py
@@ -7,5 +7,13 @@ def find_permutations(number_list, permutation_index):
             find_permutations(number_list, permutation_index + 1)
             number_list[permutation_index], number_list[i] = number_list[i], number_list[permutation_index]
 
+
+def find_cash_3(number_list):
+	if len(number_list) != 3:
+		print "not a cash 3 list"
+	else:
+		list_one = [number_list[0], number_list[1]]
+		find_permutations(list_one, 0)
+		
 test_list = [1,2,3]
 find_permutations(test_list,0)
