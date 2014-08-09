7dfee9db68a3cdec19c014a521728d7b5ff85dda
cmoneylulz <element4emerica@gmail.com>
2014-08-08 23:09:46 -0400

    added actual permutations (order matters) for cash 3, cash 4, and fantasy_five

--- permutation.py
+++ permutation.py
@@ -26,8 +26,49 @@ def find_cash_4(number_list):
 		find_permutations(list_one, 0)
 		find_permutations(list_two, 0)
 
-test_list = [1,2,3]
-find_cash_3(test_list)
+def cash_3(number_list):
+	if len(number_list) != 3:
+		print "not a valid cash 3 pick"
+	else:
+		list_one = [number_list[0], number_list[1]]
+		list_two = [number_list[1], number_list[2]]
 
-test_list2 = [1,2,3,4]
-find_cash_4(test_list2)
+		number_list.sort()
+		list_one.sort()
+		list_two.sort()
+
+		print number_list
+		print list_one
+		print list_two
+
+def cash_4(number_list):
+	if len(number_list) != 4:
+		print "not a valid cash 4 pick"
+	else:
+		list_one = [number_list[0], number_list[1], number_list[2]]
+		list_two = [number_list[1], number_list[2], number_list[3]]
+
+		number_list.sort()
+		list_one.sort()
+		list_two.sort()
+
+		print number_list
+		print list_one
+		print list_two
+
+def fantasy_5(number_list):
+	if len(number_list) != 5:
+		print "not a valid fantasy 5 pick"
+	else:
+		number_list.sort()
+		print number_list
+
+
+test_list = [3,1,2]
+cash_3(test_list)
+
+test_list = [3,2,4,1]
+cash_4(test_list)
+
+test_list = [4,2,1,5,3]
+fantasy_5(test_list)
